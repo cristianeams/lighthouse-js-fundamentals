fa71321b7272737fc0ac05cedf9bea203b4a4330
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-05-11 23:04:29 +0000

    Adds functions exercises

---
+++ even.js
@@ -0,0 +1,9 @@
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+//var tenIsEven = isEven(10); //true
+//var elevenIsEven = isEven(11); //false
+
+console.log(isEven(10));
+console.log(isEven(11));
