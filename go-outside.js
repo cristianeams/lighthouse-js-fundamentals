03992d8ac473d2a4f7c48f6df748f46f0a4baa99
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-05-14 22:38:31 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,33 @@
+// var raining = true
+// var cold = true;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// var cold = false;
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+//var temperature = 12;
+//var temperature = -1;
+var temperature = 17;
+
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
---
+++ if-statements.js
@@ -0,0 +1,12 @@
+//&& both need to be true to evaluate to true
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+//|| or, one can be false
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+//!reverses the value
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
